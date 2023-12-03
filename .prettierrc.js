a0a0dd586d05556d0f3ec0d509e89d8b18ab9e78
Kenji Matsuoka <kenjim@gmail.com>
2023-12-03 13:05:44 -0800

    update prettier*

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,10 @@
-module.exports = {
+const config = {
   arrowParens: "avoid",
   bracketSameLine: true,
   bracketSpacing: false,
+  plugins: ["prettier-plugin-organize-imports"],
   singleQuote: false,
   trailingComma: "all",
   semi: false,
 }
+module.exports = config
