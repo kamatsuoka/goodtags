71806c283360a551fe16935aded239d3a4d75f52
Kenji Matsuoka <kenjim@gmail.com>
2025-12-19 13:49:06 -0800

    fix android build

--- android/app/src/main/java/com/fogcitysingers/goodtags/MainApplication.kt
+++ android/app/src/main/java/com/fogcitysingers/goodtags/MainApplication.kt
@@ -24,6 +24,7 @@ class MainApplication : Application(), ReactApplication {
         // add(MyReactNativePackage())
         add(StatusBarPackage())
       },
+    )
   }
 
   override fun onCreate() {
