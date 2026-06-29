05ecc85b5022e6023f165dd4c9431e581a5486c7
Kenji Matsuoka <kenjim@gmail.com>
2026-06-28 18:35:55 -0700

    fix tests

---
+++ __mocks__/expo-haptics.js
@@ -0,0 +1,5 @@
+export const impactAsync = jest.fn(() => Promise.resolve())
+export const notificationAsync = jest.fn(() => Promise.resolve())
+export const selectionAsync = jest.fn(() => Promise.resolve())
+export const ImpactFeedbackStyle = { Light: 'light', Medium: 'medium', Heavy: 'heavy' }
+export const NotificationFeedbackType = { Success: 'success', Warning: 'warning', Error: 'error' }
